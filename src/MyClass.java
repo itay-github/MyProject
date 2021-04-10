240b79a216e2296cfca3f05510088d922ebb548b
itay-github <itaybachar@campus.technion.ac.i>
2021-04-10 12:57:43 +0300

    Added new int x

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,9 +1,6 @@
 public class MyClass {
     public static void main(String[] args) {
-        int y = 7;
-        int x = 8;
-        int z = 9;
-        String almog="the best partner";
-        int g = 10;
+        int x = 9;
+
     }
 }
